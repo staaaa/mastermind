303c6bc0d53aaff099579e8ad1dbef59905cb098
Stanisław Bożych <stachubozych@gmail.com>
2025-04-07 19:46:47 +0200

    added comments and additional logic to decoder

--- stachu/coder.py
+++ stachu/coder.py
@@ -4,56 +4,78 @@ from random import Random
 import multiprocessing
 import time
 
+# Class Coder - that's the player that's coding the password and marking the guesses
 class Coder(multiprocessing.Process):
     def __init__(self, conn):
         super().__init__()
         self.password = []
-        self.allPerms = list(combinations_with_replacement([1,2,3,4,5,6], 4))
+        self.allCombinations = list(combinations_with_replacement([1,2,3,4,5,6], 4))
         self.rng = Random()
         self.conn = conn
 
 
+    # Here we generate a single instance of password out of list of all possbile combinations
     def generatePassword(self): 
-        self.password = self.allPerms[self.rng.randrange(3, len(self.allPerms))]
+        self.password = self.allCombinations[self.rng.randrange(0, len(self.allCombinations))]
 
 
+    # This method takes a guess as a parameter (sent from decoder) and marks it
     def markGuess(self, guess):
-        colors = 0
-        whites = 0
+        #colors are points for correct guesses on correct places
+        colors = []
+        #whites are points for correct guesses on incorrect places
+        whites = []
+        #if color = 4 we want to indicate that the game ended
         won = False
+        #in this array we place the indexes of positions we marked in first iteration
         marked = []
 
         #1. Mark all elements that are correct color and correctly placed
+        #   Additionally, add every index that was marked to marked list
         for i in range(len(guess)):
             if guess[i] == self.password[i]:
-                colors += 1
+                colors.append((i, guess[i]))
                 marked.append(i)
 
         #2. Create remaning array with only unmarked colors from password
+        #   we do this so that we dont have a situation like this:
+        #   password generated  - 1 2 3 4
+        #   guess               - 1 1 9 9 
+        #   we mark first 1 as 'color'
+        #   the second 1 would be marked as 'white' because it would still be in password
+        #   so we have to remove every marked number from password before second marking.
         remaining = [self.password[i] for i in range(len(self.password)) if i not in marked]
 
         #3. Mark all elements that are still in guess and havent been marked previously
+        #   This way we ensure that all "colors" were marked before, and now we only
+        #   mark "whites".
         for i in range(len(guess)):
+            # if current number is in remaining password and this index hasn't been marked before
             if guess[i] in remaining and i not in marked:
-                whites += 1
+                whites.append((i, guess[i]))
                 marked.append(i)
                 remaining.remove(guess[i])
-        if colors == 4:
+        if len(colors) == 4:
             won = True
         return (won, colors, whites)
 
 
     def run(self):
-        #wait to recive the guess
+        # 1. Generate password
         self.generatePassword()
         print(f"Coder generated password: {self.password}")
+        # 2. Run untill game hasn't finished
         while True:
+            # 3. Wait to recive a guess
             guess = self.conn.recv()
             print(f"Coder got {guess} as guess")
+            # Simulate time that it would take to check the guess
             time.sleep(5)
-            mark = self.markGuess(guess)
-            self.conn.send(mark)
-            print(f"Coder send {mark} as mark")
-            if mark[0] == True:
+            # 4. Mark the guess and send back the result to the coder
+            result = self.markGuess(guess)
+            self.conn.send(result)
+            print(f"Coder send {result} as mark")
+            # 5. Check the first parameter in result (won: Boolean)
+            if result[0] == True:
                 break
         print("Decoder won")
--- stachu/decoder.py
+++ stachu/decoder.py
@@ -8,21 +8,77 @@ class Decoder(multiprocessing.Process):
         self.allPerms = list(combinations_with_replacement([1,2,3,4,5,6], 4))
         self.rng = Random()
         self.conn = conn
-    
+        self.feedback = None
+        self.previousGuess = None
+        self.numbersAvailable = [1,2,3,4,5,6]
 
     def makeGuess(self):
-        guess = self.allPerms[self.rng.randrange(0, len(self.allPerms)-1)]
-        self.allPerms.remove(guess)
+        #If we haven't recived any feedback to our previous guesses then choose randomly
+        if self.feedback is None:
+            guess = self.allPerms[self.rng.randrange(0, len(self.allPerms)-1)]
+            self.allPerms.remove(guess)
+        #If we have recived feedback then
+        else:
+            #Colors and whites are a list of tuples {index, value}
+            colors = self.feedback[1]
+            whites = self.feedback[2]
+            guess = [0,0,0,0]
+            remainingIndexes = [0,1,2,3]
+
+            #Keep the old correct guesses
+            for color in colors:
+                guess[color[0]] = color[1]
+                remainingIndexes.remove(color[0])
+
+            #Keep numbers from whites but change indexes
+            for white in whites:
+                while True:
+                    #generate random number 
+                    randomIndex = self.rng.randrange(0, len(remainingIndexes))
+                    #we will pick a index[number] and check if the index is different from previous guess
+                    if remainingIndexes[randomIndex] != white[0]:
+                        #if so set a guess[index[niumber]] to white value (change white index)
+                        guess[remainingIndexes[randomIndex]] = white[1]
+                        #remove used index from remaining indexes
+                        remainingIndexes.remove(remainingIndexes[randomIndex])
+                        break
+            
+            if self.previousGuess != None:
+                numbers_to_remove = []
+                previous_values = self.previousGuess  
+                
+                # Identify numbers to remove
+                for number in self.numbersAvailable:
+                    if (number in previous_values and 
+                        not any(color[1] == number for color in colors) and 
+                        not any(white[1] == number for white in whites)):
+                        numbers_to_remove.append(number)
+
+                # Remove identified numbers so that decoder doesnt use twice incorrect numbers
+                for num in numbers_to_remove:
+                        if num in self.numbersAvailable:
+                            self.numbersAvailable.remove(num)
+            
+            # Fill the rest of the remaining numbers with random guesses (but do not use the incorrect numbers)
+            # That were previously used
+            for index in remainingIndexes:
+                randomIndex = self.rng.randrange(0, len(self.numbersAvailable))
+                guess[index] = self.numbersAvailable[randomIndex]
+
+        self.previousGuess = guess
         return guess
     
 
     def run(self):
         while True:
+            # Make a guess and send it to coder
             guess = self.makeGuess()
             self.conn.send(guess)
             print(f"Decoder send {guess} as guess")
-            mark = self.conn.recv()
-            print(f"Decoder got {mark} as mark \n")
-            if mark[0] == True:
+            # Wait for feedback from coder
+            self.feedback = self.conn.recv()
+            print(f"Decoder got {self.feedback} as feedback \n")
+            # Check if coder said it's gameover
+            if self.feedback[0] == True:
                 break
         
